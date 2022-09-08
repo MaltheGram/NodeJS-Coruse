0cef7bdfa6993f16349b8086d077a362855d42d5
Malthe <malthegram22@gmail.com>
2022-09-08 14:56:25 +0200

    working on get

--- 1_Basic_Express/Weapons_API/app.js
+++ 1_Basic_Express/Weapons_API/app.js
@@ -1,5 +1,18 @@
 const express = require("express")
 const app = express()
+const weaponsList = {
+        "ak": {
+            "name": "ak47",
+            "rounds": 30,
+            "price" : 3000
+        },
+        "colt": {
+            "name": "Colt",
+            "rounds": 35,
+            "price": 3500
+
+        }
+}
 
 app.get("/",(req, res) => {
     res.send({
@@ -8,25 +21,19 @@ app.get("/",(req, res) => {
 })
 
 // Path variable
-
 app.get("/weapons/:id", (req, res) => {
-    if (Number(req.params.id) === 1){
-        res.send({
-            Weapon: "AK47",
-            GOAT: true
-        })
-    }
-})
+    Number(req.params.id) === 1 ? res.send({Weapon: "AK47", GOAT: true}) : res.send({errorMessage: `No weapon with given id: ${req.params.id}`})
 
+})
 // Query String
 app.get("/weapons", (req, res) => {
     res.send({
-        message: "Information about weapon",
-        Weapon: req.query
+        message: `Info about ${req.query.model}`
     })
 })
 
 app.listen(8080, () => {
     console.log("Listening on port",8080)
 })
+
 
