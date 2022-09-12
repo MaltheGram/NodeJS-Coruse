85f881d95c4c113c81877c5a5e4af1da91def6d2
Malthe <malthegram22@gmail.com>
2022-09-12 14:48:37 +0200

    working on full crud

--- Weapons_API/app.js
+++ Weapons_API/app.js
@@ -1,22 +1,24 @@
 import express from "express"
-import { logConsole } from "./Utils.js";
+import bodyParser from "body-parser"
+
 const app = express()
 
-const weapons = {
-        1: {
-            "name": "ak47",
-            "rounds": 30,
-            "price" : 3000
-        },
-        2: {
-            "name": "Colt",
-            "rounds": 35,
-            "price": 3500
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-        }
-}
+const weapons = [
+    { name: "AK-47", price: 3500 },
+    { name: "COLT", price: 3000 }
+]
 
-app.use(express.json())
+
+
+//app.use(express.json())
+
+// Get
+
 // Root
 app.get("/",(req, res) => {
     res.send({
@@ -26,7 +28,7 @@ app.get("/",(req, res) => {
 
 // Path variable
 app.get("/weapons/:id", (req, res) => {
-    Number(req.params.id) === 1 ? res.send({Weapon: "AK47", GOAT: true}) : res.send({errorMessage: `No weapon with given id: ${req.params.id}`})
+    Number(req.params.id) === 1 ? res.send(weapons[0]) : res.send({errorMessage: `No weapon with given id: ${req.params.id}`})
 
 })
 // Query String --> req.query
@@ -36,9 +38,34 @@ app.get("/weapons", (req, res) => {
     })
 })
 
+// Post
+
+app.post("/weapons", (req, res) => {
+    const weapon = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    console.log(weapon)
+    weapons.push(weapon)
+})
+
+// Update
+app.put("/weapons/:id",(req,res) => {
+    const weapon = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    console.log(weapon)
+    weapons.splice(0,0,weapon)
+})
+
+// Delete
+app.delete("/weapons/:id", (req, res) => {
+    weapons.splice(1,1)
+})
 
 app.listen(8080, () => {
-    logConsole(8080)
+    console.log("Running on port",8080)
 })
 
 
