e46375255b944d1297d9d9184485bcfc864872de
Malthe <malthegram22@gmail.com>
2022-09-26 11:23:35 +0200

    pokemon restructured

--- 4_Pokemon/app.js
+++ 4_Pokemon/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import path from "path"
+import e from "express";
 
 // Instantiate express
 const app = express()
@@ -10,15 +11,32 @@ app.use(express.static("public"))
 const pokemon = {name: "Pikachu"}
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("./public/frontpage.html"))
+    res.sendFile(path.resolve("./public/frontpage/frontpage.html"))
 })
 
-app.get("/pokemon", (req, res) => {
-    res.send({data: pokemon})
+app.get("/battle", (req, res) => {
+    res.sendFile(path.resolve("./public/battle/battle.html"))
 })
 
+app.get("/api/pokemon", (req, res) => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+        .then(result => result.json())
+        .then(result => res.send({data: result}))
+})
 
+const PORT = process.env.PORT || 8080
+
+console.log(process.env.PORT)
+
+const server = app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    console.log("Server is running on port,", server.address().port)
+})
+
+/*
 app.listen(8080, (error) => {
     console.log(error)
     console.log("Listening on port", 8080)
-})
+})*/
---
+++ 4_Pokemon/public/frontpage/frontpage.js
@@ -0,0 +1,31 @@
+
+
+
+// fetch("/pokemon")
+//     .then(response => response.json())
+//     .then(result => console.log(result))
+//
+//
+// const pokemonDiv = document.getElementById("pokemon")
+// console.log(pokemonDiv)
+//
+
+// fetch("/pokemon")
+//     .then(response => response.json())
+//     .then(result => {
+//         result.data.results.forEach(pokemon => {
+//             const pokemonIndividualDiv = document.createElement("div")
+//             const pokemonName = document.createElement("p")
+//             pokemonName.innerText = pokemon.name
+//
+//             pokemonIndividualDiv.appendChild(pokemonName)
+//             pokemonDiv.appendChild(pokemonIndividualDiv)
+//
+//         })
+//     })
+
+// Redirect after X time
+// setTimeout(() => {
+//     window.location.href = "/battle"
+//
+// },3000)
